d1c47c0d8078c5b90b8c3e31b390f8ad26311947
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-06-03 17:06:35 +0000

    min 55

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,14 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+@api.route('/token', methods=['POST'])
+def create_token():
+    email=request.json.get('email', None)
+    password=request.json.get('password', None)
+
+    user = User.query.filter_by(email = email, password = password).first()
+    user = user.serialize()
+    token = create_access_token(identity = user.id)
+
+
+    return jsonify({'token':token, 'user': user}), 200
